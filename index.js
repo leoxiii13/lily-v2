4081653617fb1530ad4ce1819c390fed4e1a85a4
leoxiii13 <leoxiii13@gmail.com>
2021-07-19 14:34:18 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = ".";
+
+client.on("ready", () => {
+  console.log(`Log in successful! Welcome back, ${client.user.tag}`);
+});
+
+client.on("message", msg => {
+  if(msg.content==="test"){
+    msg.channel.send("TEST DEEZ NUTS");
+  }    
+});
